778991dc1546f7c1ceaecdf168f17e6119b17291
izael.junior@icloud.com <jucb05@gmail.com>
2022-08-22 23:30:41 -0300

    Angular - Interpolation and Binding

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { SpeakerComponent } from './speaker/speaker.component';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import {CollapseModule} from 'ngx-bootstrap/collapse';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +24,8 @@ import {CollapseModule} from 'ngx-bootstrap/collapse';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    CollapseModule.forRoot()
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/ProEventos-App/src/app/events/events.component.ts
+++ Front/ProEventos-App/src/app/events/events.component.ts
@@ -9,6 +9,29 @@ import { Component, OnInit } from '@angular/core';
 export class EventsComponent implements OnInit {
 
   public events: any = [];
+  public filteredEvents: any = [];
+
+  widthImage: number = 150;
+  marginImage: number = 2;
+  showImage: boolean = true;
+  private _filterList: string = '';
+
+  public get filterList(): string {
+    return this._filterList;
+  }
+
+  public set filterList(value: string) {
+    this._filterList = value;
+    this.filteredEvents = this.filterList ? this.filterEvents(this.filterList) : this.events;
+  }
+
+  filterEvents(filterBy: string): any {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.events.filter(
+      (event: any) => event.theme.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+      event.local.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,11 +39,18 @@ export class EventsComponent implements OnInit {
     this.getEvents();
   }
 
+  changeImage(){
+    this.showImage = !this.showImage;
+  }
+
   public getEvents(): void
   {
-    /*this.http.get('https://localhost:5001/api/event').subscribe(
-      response => this.events = response,
+    this.http.get('https://localhost:5001/api/event').subscribe(
+      response => {
+        this.events = response;
+        this.filteredEvents = this.events;
+      },
       error => console.log(error),
-    );*/
+    );
   }
 }
